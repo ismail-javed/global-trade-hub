df4ec577affe3981989f478e1ee478d8f2870285
Ismail-Javed <ismailjaved02@gmail.com>
2026-04-20 00:49:32 +0530

    nothing major

--- src/component/Body/indexBody/Flags.jsx
+++ src/component/Body/indexBody/Flags.jsx
@@ -4,7 +4,7 @@
     return (
         <div className="flags-strip">
         <div className="flags-strip-inner">
-            <span className="flags-label">Delivering to 10+ Countries</span>
+            <span className="flags-label">Delivering to 27+ Countries</span>
 
             <div className="flags-divider"></div>
 
@@ -30,19 +30,58 @@
 
                 {/* duplicate for seamless loop */}
                 <div className="flag-item">
-                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1775757987/USA_cixmvc.png" alt="USA" />
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776618035/Australia_fdhrqb.png" alt="australia" />
                 </div>
                 <div className="flag-item">
-                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1775757988/uk_zfovql.png" alt="UK" />
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776618861/Saudi_pnel2w.png" alt="Saudi Arabia" />
                 </div>
                 <div className="flag-item">
-                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776264005/UAE_i7acta.png" alt="UAE" />
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776618861/ireland_l2xebs.png" alt="Ireland" />
                 </div>
                 <div className="flag-item">
-                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1775757986/canada_usiclm.png" alt="Canada" />
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776618861/Belgium_h48rqq.png" alt="Belgium" />
                 </div>
                 <div className="flag-item">
-                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1775757986/INDIA_n2dxda.png" alt="India" />
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776620119/Turkey_t4my9c.png" alt="Turkey" />
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776620365/Austria_slcmpw.png" alt="Austria" />
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776620688/Bulgaria_v81a3n.png" alt="Austria" />
+                
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776620822/Hungry_pvdtrw.png" alt="Hungry" />
+                </div>
+
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776621834/Sweden_scl60z.png" alt="Sweden" />
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776621833/Spain_fk0enu.png" alt="Spain" />
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776621832/Slovenia_imuidi.png" alt="Slovenia" />
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776622141/Finland_noe98t.png" alt="Finland" />
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776621831/Slovakia_hjmf3x.png" alt="Slovakia" />
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776621829/romania_sjabai.png" alt="Romania" />
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776621828/portugal_izofzb.png" alt="Portugal" />
+                </div>
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776621827/poland_t17ilo.png.png" alt="Poland" />
+                </div>
+
+                <div className="flag-item">
+                <img src="https://res.cloudinary.com/dyb9hlxzq/image/upload/v1776621827/France_oi6tj8.png" alt="France" />
                 </div>
 
             </div>
