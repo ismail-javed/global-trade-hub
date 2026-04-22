15c4e8350d8dd49bb60df8bf7d4f9f8e57fb9faf
Ismail-Javed <ismailjaved02@gmail.com>
2026-04-22 20:55:12 +0530

    updated flags API and admin panel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { fileURLToPath, URL } from 'node:url'
+import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
